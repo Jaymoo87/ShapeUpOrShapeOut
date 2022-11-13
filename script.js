402a9a46b8e2af32350a97dddb9819b3c6e609d5
Justin Murrah <jdmurrah@gmail.com>
2022-11-12 23:56:45 -0600

    worked out some language issues and got some circles poppin up'

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+//document.addEventListener(DOMContentLoaded, function () {
+//-----Variables-----//
+// variable for the container all of the shapes will go into
+let shapeContainer = document.getElementById("shapeContainer");
+// variable for all of the buttons that will spawn a new shape.
+let circleButton = document.getElementById("circleButton");
+let squareButton = document.getElementById("#triangleButton");
+let triangleButton = document.getElementById("#triangleButton");
+
+let radiusInput = document.getElementById("radiusInput");
+// define and establish maxSize variable
 let maxSize = 600;
+//creates an array to put the generated shapes into
 let arrayOfShapes = [];
 let randomValue = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
@@ -8,23 +20,27 @@ class Shape {
   constructor(height, width) {
     this.height = height;
     this.width = width;
-    this.div = $(`<div class="${this.constructor.name}"></div>`);
 
     /// sets conditions if the value exceeds maxValue
     if (this.height > maxSize && this.height > maxSize) {
-      alert(`Whoa you think that's gonna fit!?!`);
+      alert("Whoa you think that's gonna fit!?!");
     } else if (this.height > maxSize) {
-      alert(`That's just too tall. The other shapes will feel inferior.`);
+      alert("That's just too tall. The other shapes will feel inferior.");
     } else if (this.width > maxSize) {
       alert(
-        `That's quite the girth! let's try a little less, it's just not gonna fit.`
+        "That's quite the girth! let's try a little less, it's just not gonna fit."
       );
       // if it doesn't meet the conditions that violate the max size
     } else {
-      this.div.style.width = `${this.width}px`;
+      //this.div = $(`<div class="${this.constructor.name}"></div>`);
+      this.div = document.createElement("div");
       this.div.style.height = `${this.height}px`;
-      $("#shapeContainer").append(this.div);
+      this.div.style.width = `${this.width}px`;
+      shapeContainer.appendChild(this.div);
+      console.log(this);
+
       arrayOfShapes.push(this);
+      this.randomLocation();
     }
   }
   //creates a random location to place the new shape div
@@ -38,23 +54,34 @@ class Shape {
 class Circle extends Shape {
   constructor(height) {
     super(height, height);
-    this.height = this.width;
+    //this.height = height;
+    this.width = height;
     this.radius = height / 2;
+    this.div.classList.add("circle");
   }
 }
+
 class Triangle extends Shape {
   constructor(height, width) {
     super(height, width);
+    this.div.classList.add("triangle");
   }
 }
 class Square extends Shape {
-  constructor(height, width, side) {
+  constructor(height, width) {
     super(height, width);
-    this.side = side;
+    this.div.classList.add("square");
+    //this.side = side;
+  }
+  addSquare() {
+    this.div(this.height, this.height);
+    //
   }
 }
-$("#circleButton").click(() => {
-  console.log(new Circle(25));
-});
 
+circleButton.addEventListener("click", () => {
+  new Circle(radiusInput.value * 2);
+  //shapeContainer.appendChild(newcircle);
+});
+//});
 //randomColor;
